1187f1ac5cc2e20886e718b8eccc00483c026c4a
Ujjwal-21-AI <kumarsinhaujjwal5@gmail.com>
2026-07-30 19:00:00 +0530

    Basics of recursion

---
+++ Recursionbasic.java
@@ -0,0 +1,9 @@
+public class Recursionbasic {
+    static void printmessage(){
+        System.out.println("Hello World!!");
+        printmessage();
+    }
+    public static void main(String[] args){
+        printmessage();
+    }
+}
