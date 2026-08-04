03e48ec7545be866501d71ed49082ee344b985dc
Ujjwal-21-AI <kumarsinhaujjwal5@gmail.com>
2026-08-04 19:47:29 +0530

    Shorting

---
+++ Selection.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Selection{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<=n-2; i++){
+            int mini = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[mini]){
+                    mini = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[mini];
+            arr[mini] = temp;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
