dbf91e061e649ddfe2dababb220f8cf57d44abe9
Ujjwal-21-AI <kumarsinhaujjwal5@gmail.com>
2026-08-24 21:52:05 +0530

    Continuing Array

--- Missing.java
+++ Missing.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 public class Missing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,20 +7,18 @@ public class Missing {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        int target = 0;
-        for (int i = 0; i <= n+1; i++) {
-            target = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == i) {
-                    target = 1;
-                    break;
-                }
-            }
-            if (target == 0) {
-                System.out.println(i);
-                return;
-            }
+
+        int xor = 0;
+
+        for (int i = 1; i <= n + 1; i++) {
+            xor = xor ^ i;
         }
+
+        for (int i = 0; i < n; i++) {
+            xor = xor ^ arr[i];
+        }
+
+        System.out.println(xor);
         sc.close();
     }
 }
