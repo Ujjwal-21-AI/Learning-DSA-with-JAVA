64952173e1dbbd39e05ed799c42f97e06c9fa641
Ujjwal-21-AI <kumarsinhaujjwal5@gmail.com>
2026-08-25 19:12:39 +0530

    Basics of Array Done

--- FindnocaOnce.java
+++ FindnocaOnce.java
@@ -1,24 +1,18 @@
 import java.util.*;
-public class Linear {
+public class FindnocaOnce {
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
+        int XOR = 0;
 
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-
-        int target = sc.nextInt();
-
         for(int i=0; i<n; i++){
-            if(arr[i] == target){
-                System.out.println(i);
-                return;
-            }
+            XOR = XOR^arr[i];
         }
-        System.out.println("-1");
+        System.out.println(XOR);
         sc.close();
-    }
+    } 
 }
---
+++ MaxConOne.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MaxConOne{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int maxi = 0;
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                count++;
+                maxi = Math.max(maxi, count);
+            }else{
+                count = 0;
+            }
+        }
+        System.out.println(maxi);
+        sc.close();
+    }
+}
